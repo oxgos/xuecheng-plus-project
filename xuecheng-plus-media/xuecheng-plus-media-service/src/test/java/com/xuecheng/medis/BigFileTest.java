6c4e5518f36b72ca3f0e027ce0fca48d2bf7270a
_SSO_T_22667@csair.com <_SSO_T_22667@csair.com>
2024-02-04 23:08:42 +0800

    feat: 添加注解及testRandomAccessFile测试

--- xuecheng-plus-media/xuecheng-plus-media-service/src/test/java/com/xuecheng/medis/BigFileTest.java
+++ xuecheng-plus-media/xuecheng-plus-media-service/src/test/java/com/xuecheng/medis/BigFileTest.java
@@ -30,7 +30,9 @@ public class BigFileTest {
         int chunkSize = 1024 * 1024 * 5;
         // 分块文件个数
         int chunkNum = (int) Math.ceil(sourceFile.length() * 1.0 / chunkSize);
-        // 使用流从源文件读数据，向分块文件中写数据, RandomAccessFile随机流可写可读
+        // 使用流从源文件读数据，向分块文件中写数据
+        // RandomAccessFile随机流可写可读
+        // 源数据读取流
         RandomAccessFile raf_r = new RandomAccessFile(sourceFile, "r");
         // 缓存区
         byte[] bytes = new byte[1024];
@@ -39,7 +41,9 @@ public class BigFileTest {
             // 分块写入流
             RandomAccessFile raf_rw = new RandomAccessFile(chunkFile, "rw");
             int len = -1;
+            // raf_r.read(bytes)作用：把bytes大小的数据读取到bytes里，如果没有数据，返回则为-1，否则返回读取数据大小
             while ((len = raf_r.read(bytes)) != -1) {
+                // raf_rw.write(bytes, 0, len)作用：把在bytes里从0至len位置数据，写入chunkFile
                 raf_rw.write(bytes, 0, len);
                 if (chunkFile.length() >= chunkSize) {
                     break;
@@ -48,7 +52,6 @@ public class BigFileTest {
             raf_rw.close();
         }
         raf_r.close();
-        ;
     }
 
     // 测试合并
@@ -113,5 +116,31 @@ public class BigFileTest {
 
     public boolean isNumeric(String str) {
         return str != null && str.matches("\\d+");
+    }
+
+    @Test
+    public void testRandomAccessFile() {
+        // 源文件
+        File sourceFile = new File("/Users/gavin_guo/Desktop/study-demo/backend/java/test-big-file/01. from the Heart.mp4");
+        // 写入文件
+        File file = new File("/Users/gavin_guo/Desktop/study-demo/backend/java/test-big-file/test.mp4");
+        try (
+                RandomAccessFile raf_r = new RandomAccessFile(sourceFile, "r");
+                RandomAccessFile raf_rw = new RandomAccessFile(file, "rw");
+        ) {
+            // 每次读取1024字节的buffer
+            byte[] bytes = new byte[1024];
+            int len;
+            // 利用raf_r.read(bytes)读取1024字节数据，如果没有数据，返回则为-1，否则返回读取数据大小
+            while ((len = raf_r.read(bytes)) != -1) {
+                // // 0和len均表示: 从bytes的0位置到len位置写入到mergeFile里
+                raf_rw.write(bytes, 0, len);
+                if (file.length() >= sourceFile.length()) {
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
